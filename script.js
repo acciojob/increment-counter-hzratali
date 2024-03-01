d1b7f399f4d1c741f1c425277dbe091ecf4b0cc5
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-02 01:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const counterElement = document.getElementById('counter');
 const incrementBtn = document.getElementById('incrementBtn');
 // Add click event listener to the button
 incrementBtn.addEventListener('click', function() {
-	// Get the current counter value and convert it to a number
-	let counterValue = parseInt(counterElement.innerText);
-	// Increment the counter value
-	counterValue++;
-	// Update the counter element with the new value
-	counterElement.innerText = counterValue;
-	// Display an alert with the current counter value
-	alert("Current Counter Value: " + counterValue);
+    // Get the current counter value and convert it to a number
+    let counterValue = parseInt(counterElement.innerText);
+    // Display an alert with the current counter value
+    alert("Current Counter Value: " + counterValue);
+    // Increment the counter value
+    counterValue++;
+    // Update the counter element with the new value
+    counterElement.innerText = counterValue;
 });
